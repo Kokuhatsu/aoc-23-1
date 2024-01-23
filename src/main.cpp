ae65a8b7384882092bbbfefe0db1b21852cff7ba
Kokuhatsu <thompson99305+gh@gmail.com>
2024-01-23 17:03:57 +0100

    🚀 Part I

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 // Copyright (C) 2024 Kokuhatsu
 // SPDX-License-Identifier: MIT
 
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "line_parsers.hpp"
+
 int main() {
+  std::ifstream input_file(
+    std::filesystem::path(__FILE__).remove_filename()
+    / ".."
+    / "input.txt");
+
+  std::int64_t result = 0;
+
+  while(input_file.eof() == false) {
+    std::string line;
+    input_file >> line;
+
+    if(line.empty()) break;
+
+    result += aoc231::get_value_from_line(line);
+  }
+
+  std::cout << "result: " << result << std::endl;
+
   return 0;
 }
