7234455fcf317a688ed48fc79f2e55adf97692b1
Kokuhatsu <thompson99305+gh@gmail.com>
2024-01-23 13:50:48 +0100

    🚧 Fix compilation error

---
+++ src/line_parsers.hpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2024 Kokuhatsu
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace aoc231 {
+  std::int64_t get_value_from_line(std::string line_to_parse);
+}
