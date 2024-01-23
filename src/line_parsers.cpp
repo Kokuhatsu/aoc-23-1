14d5ecc1b849c746ed426013257c823a96cca99c
Kokuhatsu <thompson99305+gh@gmail.com>
2024-01-23 14:01:26 +0100

    🚧 Fix link error

---
+++ src/line_parsers.cpp
@@ -0,0 +1,10 @@
+// Copyright (C) 2024 Kokuhatsu
+// SPDX-License-Identifier: MIT
+
+#include "line_parsers.hpp"
+
+namespace aoc231 {
+  std::int64_t get_value_from_line(std::string line) {
+    return 0;
+  }
+}
